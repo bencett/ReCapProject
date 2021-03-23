79ebc83941ce1f3a3e61d496b42f1169a7b6c0e4
Çetin Ürün <cetinurun.12@outlook.com>
2021-03-23 16:39:05 +0300

    Add project files.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    class Car:IEntity
+    {
+        public int Id { get; set; }
+        public string BrandId { get; set; }
+        public string ColorId { get; set; }
+        public DateTime ModelYear { get; set; }
+        public double DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
